7bc0204d018f4d2c94e475027cd8955ba453d0e2
SHANE <sam6091260@gmail.com>
2023-05-26 17:12:09 +0800

    RWD

--- app.js
+++ app.js
@@ -5,79 +5,110 @@ $(function () {
 });
 
 const hbgBtn = document.querySelector(".hbgIcon");
-const nav = document.querySelector(".navbar");
+const nav = document.querySelector("ul");
 hbgBtn.addEventListener("click", () => {
   nav.classList.toggle("open");
 });
 
-$(document).ready(function () {
-  //initialize swiper when document ready
-  var mySwiper = new Swiper(".swiper-container", {
-    // Optional parameters
-    direction: "vertical",
-    loop: true,
-  });
+// $(document).ready(function () {
+//   //initialize swiper when document ready
+//   var mySwiper = new Swiper(".swiper-container", {
+//     // Optional parameters
+//     direction: "vertical",
+//     loop: true,
+//   });
+// });
+
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
 });
 
-// axios
-//   .get("https://2023-engineer-camp.zeabur.app/api/v1/works")
-//   .then(function (res) {
-//     console.log(res.data);
-//   });
-
 // 資料串接
-const apiPath = "https://2023-engineer-camp.zeabur.app";
-const list = document.querySelector("#list");
+// const apiPath =
+//   "https://soa.tainan.gov.tw/Api/Service/Get/b1054400-86a4-40fa-aea0-fb39fce361e2";
+// const list = document.querySelector("#list");
 
-const data = {
-  type: "",
-  sort: 0,
-  page: 1,
-  search: "",
-};
+// const data = {
+// type: "",
+// sort: 0,
+// page: 1,
+// search: "",
+//   school: "",
+//   num: "",
+// };
+// type, sort, page, search
 
-let worksData = [];
-let pagesData = {};
+// let worksData = [];
+// let pagesData = {};
 
-function getData({ type, sort, page, search }) {
-  const apiUrl = `${apiPath}/api/v1/works?sort=${sort}&page=${page}&${
-    type ? `type=${type}&` : ""
-  }${search ? `search=${search}` : ""}`;
-  axios.get(apiUrl).then((res) => {
-    worksData = res.data.ai_works.data;
-    pagesData = res.data.ai_works.page;
+// function getData({ school, num }) {
+//   const apiUrl = `${apiPath}/?sort=${school}&num=${num}`;
+//   axios.get(apiUrl).then((res) => {
+//     worksData = res.data.ai_works.data;
+//     pagesData = res.data.ai_works.page;
 
-    renderWorks();
-  });
-}
-
-getData(data);
+//     renderWorks();
+//   });
+// }
+// getData(data);
 
 // 作品選染至畫面
-function renderWorks() {
-  let works = "";
+// function renderWorks() {
+//   let works = "";
 
-  worksData.forEach((item) => {
-    works += /*html*/ `<li>
-    <div class="card-img">
-      <img
-        src="${item.imageUrl}" alt="ai image"
-      />
-    </div>
-      <h3 class="card-title">${item.title}</h3>
-      <p class="cardP">${item.description}</p>
-    <div class="card-item">
-      <p class="card-subtitle">AI 模型</p>
-      <p class="card-text">${item.model}</p>
-    </div>
-    <div class="card-item">
-        <span class="card-text">#${item.type}</span>
-        <a class="card-link" href="${item.link}" target="_blank">
-        <span class="material-icons"> share</span>
-    </div>
-  </li>
-  `;
+//   worksData.forEach((item) => {
+//     works += /*html*/ `<li>
+//     <div class="card-img">
+//       <img
+//         src="${item.imageUrl}" alt="ai image"
+//       />
+//     </div>
+//       <h3 class="card-title">${item.title}</h3>
+//       <p class="cardP">${item.description}</p>
+//     <div class="card-item">
+//       <p class="card-subtitle">AI 模型</p>
+//       <p class="card-text">${item.model}</p>
+//     </div>
+//     <div class="card-item">
+//         <span class="card-text">#${item.type}</span>
+//         <a class="card-link" href="${item.link}" target="_blank">
+//         <span class="material-icons"> share</span>
+//     </div>
+//   </li>
+//   `;
+//   });
+
+//   list.innerHTML = works;
+// }
+
+axios
+  .get(
+    "https://soa.tainan.gov.tw/Api/Service/Get/b1054400-86a4-40fa-aea0-fb39fce361e2"
+  )
+  .then((response) => {
+    const data = response.data;
+    console.log(data);
+
+    const totalCount = response.data.data.length;
+    console.log("臺南市總共有", totalCount, "個國小");
+
+    // 取得大甲國小的學生人數
+    const schools = response.data.data;
+    const daJiaSchool = schools.find(
+      (school) => school.SchoolName === "大甲國小"
+    );
+    if (daJiaSchool) {
+      const studentCount = daJiaSchool.NumberOfPeople;
+      console.log("大甲國小學生人數:", studentCount);
+    } else {
+      console.log("找不到大甲國小的資料");
+    }
+  })
+  .catch((error) => {
+    console.log("發生錯誤:", error);
   });
-
-  list.innerHTML = works;
-}
